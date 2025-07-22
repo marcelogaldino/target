5135c7727799a47623c699832190ea755e51cdfb
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2025-07-21 21:38:09 -0300

    feat: added summary component

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -4,6 +4,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 import { Separator } from "../Separator";
+import { Summary } from "../Summary";
 
 export type HomeHeaderProps = {
   total: string;
@@ -25,6 +26,19 @@ export function HomeHeader({ data }: Props) {
       </View>
 
       <Separator color={colors.blue[400]} />
+
+      <View style={styles.summary}>
+        <Summary
+          data={{ label: "Entradas", value: "R$ 860,20" }}
+          icon={{ name: "arrow-upward", color: colors.green[500] }}
+        />
+
+        <Summary
+          isLeft
+          data={{ label: "Saidas", value: "R$ 460,20" }}
+          icon={{ name: "arrow-downward", color: colors.red[400] }}
+        />
+      </View>
     </LinearGradient>
   );
 }
