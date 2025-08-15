f9201fb33708589b32f9ed9f4a22a50c1ae9cbbd
Marcelo Galdino <marcelinhomgo@gmail.com>
2025-08-15 18:59:50 -0300

    feat: database SQLite setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Stack } from "expo-router";
 import { colors } from "@/theme/colors";
 import {
@@ -8,6 +8,8 @@ import {
   Inter_700Bold,
 } from "@expo-google-fonts/inter";
 import { Loading } from "@/components/Loading";
+import { SQLiteProvider } from "expo-sqlite";
+import { migrate } from "@/database/migrate";
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -21,11 +23,15 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.white },
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   );
 }
--- src/app/target.tsx
+++ src/app/target.tsx
@@ -2,9 +2,50 @@ import { Button } from "@/components/Button";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { PageHeader } from "@/components/PageHeader";
-import { View } from "react-native";
+import { router } from "expo-router";
+import { useLocalSearchParams } from "expo-router/build/hooks";
+import { useState } from "react";
+import { Alert, View } from "react-native";
 
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(null);
+
+  const params = useLocalSearchParams<{ id?: string }>();
+
+  function handleSave() {
+    if (!name.trim() && amount <= 0) {
+      return Alert.alert(
+        "Atenção",
+        "Preencha nome e valor. O valor não pode ser zero",
+      );
+    }
+
+    setIsProcessing(true);
+
+    if (params.id) {
+      // uopdtae
+    } else {
+      create();
+    }
+  }
+
+  async function create() {
+    try {
+      Alert.alert("Nova Meta", "Meta criada com sucesso!", [
+        {
+          text: "OK",
+          onPress: () => router.back(),
+        },
+      ]);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possivel criar a meta");
+      console.log(error);
+      setIsProcessing(false);
+    }
+  }
+
   return (
     <View
       style={{
@@ -25,13 +66,20 @@ export default function Target() {
         <Input
           label="Nome da meta"
           placeholder="Ex: Viagem para praia, Apple Watch..."
+          onChangeText={setName}
+          value={amount}
         />
         <CurrencyInput
+          placeholder="R$ 0,00"
           label="Valor alvo (R$)"
-          value={1250.5}
-          onChangeValue={() => {}}
+          value={amount}
+          onChangeValue={setAmount}
         />
-        <Button title="Salvar" />
+        <Button
+          title="Salvar"
+          isProcessing={isProcessing}
+          onPress={handleSave}
+        />
       </View>
     </View>
   );
