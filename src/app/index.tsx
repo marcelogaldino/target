4f6088e0319f63aad07ab7f108faac6cd4944fdf
Marcelo Galdino <marcelinhomgo@gmail.com>
2025-08-26 06:15:35 -0300

    feat(Home): listing targets

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/Button";
 import { HomeHeader } from "@/components/HomeHeader";
 import { List } from "@/components/List";
-import { Target } from "@/components/Target";
+import { Target, TargetProps } from "@/components/Target";
 import { useTargetDatabase } from "@/database/useTargetDatabase";
 import { router, useFocusEffect } from "expo-router";
 import { Alert, StatusBar, View } from "react-native";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import { Loading } from "@/components/Loading";
 
 const summary = {
   total: "R$ 2.680,00",
@@ -13,41 +14,47 @@ const summary = {
   output: { label: "Saidas", value: "-R$ 460,20" },
 };
 
-const targets = [
-  {
-    id: "1",
-    percentage: "75%",
-    current: "R$ 900,00",
-    name: "Comprar cadeira",
-    target: "R$ 1.200,00",
-  },
-  {
-    id: "2",
-    percentage: "75%",
-    current: "R$ 900,00",
-    name: "Comprar cadeira",
-    target: "R$ 1.200,00",
-  },
-];
-
 export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [targets, setTargets] = useState<TargetProps[]>([]);
   const targetDatabase = useTargetDatabase();
 
-  async function fetchTargets() {
+  async function fetchTargets(): Promise<TargetProps[]> {
     try {
       const res = await targetDatabase.listBySavedValue();
       console.log(res);
+      return res.map((item) => ({
+        id: String(item.id),
+        name: item.name,
+        current: String(item.current),
+        percentage: item.percentage.toFixed(0) + "%",
+        target: String(item.amount),
+      }));
     } catch (error) {
       Alert.alert("Erro", "Nao foi possivel carregar as metas");
       console.log(error);
     }
   }
 
+  async function fetchData() {
+    const targetDataPromise = fetchTargets();
+
+    const [targetData] = await Promise.all([targetDataPromise]);
+
+    setTargets(targetData);
+    setIsLoading(false);
+  }
+
   useFocusEffect(
     useCallback(() => {
-      fetchTargets();
+      fetchData();
     }, []),
   );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <View
       style={{
